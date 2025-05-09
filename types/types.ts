366c7f973d0b2d6025a314ab3f6ecf81d0a1141f
MarekBednar <marek.bednar1995@gmail.com>
2025-05-09 03:21:33 +0200

    WIP: Make Chat Work

--- types/types.ts
+++ types/types.ts
@@ -1,5 +1,11 @@
+import { Request } from 'express';
+
 export type ServerError = {
   log: string;
   status: number;
   message: { err: string };
 };
+
+export interface FileRequest extends Request {
+  file?: Express.Multer.File;
+}
